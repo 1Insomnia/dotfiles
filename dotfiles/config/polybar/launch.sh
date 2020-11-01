8198fb2feb370faf7424428edd453aca8b6ce804
prim0rdial <jeremy.pro.lp@gmail.com>
2020-11-01 21:55:09 +0100

    again distro hoping

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar using default config location ~/.config/polybar/config
+#polybar -rq -c /home/prim0rdial/.config/polybar/config.ini main &
 polybar -rq -c /home/prim0rdial/.config/polybar/fancy.ini main &
 
 echo "Polybar launched..."
